5114bf5c703ecac137399fb2f347768811d7d5ae
Aleksandr <113410543+hammerztein@users.noreply.github.com>
2024-09-23 07:22:57 +0300

    Wrap webpages logic in the anonymous IIFE for encapsulation. Initiate pages main view.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,32 @@
 // CSS reset as a side effect
 import '../css/reset.css';
 import '../css/styles.css';
+import { createHomeSection } from './home.js';
+
+(() => {
+	const container = document.querySelector('.container');
+	const homeBtn = document.querySelector(".navbar button[data-target='home']");
+
+	const attachEventHandlers = () => {
+		homeBtn.addEventListener('click', loadHomePage);
+	};
+
+	const clearContainer = () => {
+		// Check if main container has children
+		if (container.children.length > 0) {
+			// Clear container
+			container.innerHTML = '';
+		}
+	};
+
+	const loadHomePage = () => {
+		clearContainer();
+		const homeSection = createHomeSection();
+		container.appendChild(homeSection);
+	};
+
+	loadHomePage();
+	attachEventHandlers();
+})();
+
 console.log('Script Attached');
