7b5aea675ffeb832e64e47f417e7f33bffebe0cf
Aleksandr <113410543+hammerztein@users.noreply.github.com>
2024-12-09 22:28:40 +0200

    Separate data from HTML structure to follow same convention as menu creation adding modularity and flexibility into code.

--- src/js/home.js
+++ src/js/home.js
@@ -2,6 +2,27 @@ import '../css/home.css';
 import introImageOne from '../assets/images/hero-image-1.png';
 import introImageTwo from '../assets/images/hero-image-2.jpg';
 
+const data = [
+	{
+		title: 'Rebel against tradition, taste the revolution',
+		introOne:
+			"At Ramen Rebellion, we don't just serve ramen – we break the rules. Our bowls are a rebellion against the common man, fearlessly blending age-old Japanese techniques with new flavors. From sweet and savory juices to hand-pulled noodles, we take comfort food and give it a bold and spicy twist to the top.",
+		introTwo:
+			'Whether you indulge in our fiery "Rebel Red" or indulge in the heroic vegan "Green Samurai", every bite is an act of defiance. Go inside, embrace the chaos and taste the ramen revolution!',
+		buttonText: 'Book a table',
+		image: introImageOne,
+	},
+	{
+		title: 'Who we are: Rebels in the ramen cause',
+		introOne:
+			'The ramen rebellion was born out of a passionate love for ramen and a hunger to shake things up. Delving into the depth of Japanese ramen culture, our founders decided to go beyond tradition and create something unapologetically exciting. We mix the soul of classic ramen with rebellious flavor, pushing the boundaries with every bowl we serve.',
+		introTwo:
+			"Our menu reflects this spirit – from spicy, unconventional broths to vegan options, we've got something for every rebel out there. We are all about community, taste, and rule breaking. Welcome to the Ramen Rebellion – where ramen isn't just food, it's a movement!",
+		buttonText: 'Learn More...',
+		image: introImageTwo,
+	},
+];
+
 const createCard = ({ title, introOne, introTwo, buttonText, image }) => {
 	const card = document.createElement('article');
 	const cardBody = document.createElement('div');
@@ -42,28 +63,10 @@ export const createHomeSection = () => {
 	const homeSection = document.createElement('section');
 	homeSection.id = 'home';
 
-	const cardOneText = {
-		title: 'Rebel against tradition, taste the revolution',
-		introOne:
-			"At Ramen Rebellion, we don't just serve ramen – we break the rules. Our bowls are a rebellion against the common man, fearlessly blending age-old Japanese techniques with new flavors. From sweet and savory juices to hand-pulled noodles, we take comfort food and give it a bold and spicy twist to the top.",
-		introTwo:
-			'Whether you indulge in our fiery "Rebel Red" or indulge in the heroic vegan "Green Samurai", every bite is an act of defiance. Go inside, embrace the chaos and taste the ramen revolution!',
-		buttonText: 'Book a table',
-		image: introImageOne,
-	};
-	const cardTwoText = {
-		title: 'Who we are: Rebels in the ramen cause',
-		introOne:
-			'The ramen rebellion was born out of a passionate love for ramen and a hunger to shake things up. Delving into the depth of Japanese ramen culture, our founders decided to go beyond tradition and create something unapologetically exciting. We mix the soul of classic ramen with rebellious flavor, pushing the boundaries with every bowl we serve.',
-		introTwo:
-			"Our menu reflects this spirit – from spicy, unconventional broths to vegan options, we've got something for every rebel out there. We are all about community, taste, and rule breaking. Welcome to the Ramen Rebellion – where ramen isn't just food, it's a movement!",
-		buttonText: 'Learn More...',
-		image: introImageTwo,
-	};
+	data.forEach((item) => {
+		const homeItem = createCard(item);
+		homeSection.appendChild(homeItem);
+	});
 
-	const cardOne = createCard(cardOneText);
-	const cardTwo = createCard(cardTwoText);
-
-	homeSection.append(cardOne, cardTwo);
 	return homeSection;
 };
