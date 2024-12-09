34c7e21cb3c7e655d8bdd436ba7398f19b7a1f0a
Aleksandr <113410543+hammerztein@users.noreply.github.com>
2024-12-09 22:27:52 +0200

    Create menu section using JavaScript. Separate data from HTML structure creation to add modularity and flexibility.

--- src/js/menu.js
+++ src/js/menu.js
@@ -5,7 +5,70 @@ import smokehouseRamen from '../assets/images/smokehouse-ramen.jpeg';
 import temptestVeggineRamen from '../assets/images/temptest-veggie-ramen.jpeg';
 import tsunamiTonkotsu from '../assets/images/tonkotsu.jpeg';
 import yuzuCitrus from '../assets/images/yuzu-citrus.jpeg';
-console.log(tsunamiTonkotsu);
+
+const data = [
+	{
+		image: tsunamiTonkotsu,
+		title: 'Tsunami Tonkotsu',
+		description:
+			'An Oceanic Take on Tradition – Dive into a smooth, ocean-inspired broth infused with seaweed and dashi. The Tsunami Tonkotsu is topped with marinated chashu pork, soft-boiled egg, shrimp tempura, scallions, sesame seeds, and a drizzle of spicy mayo for a burst of flavor.',
+		price: {
+			euro: '13.50',
+			yen: '2,000',
+		},
+	},
+	{
+		image: blazeMiso,
+		title: 'Blaze Miso Ramen',
+		description:
+			'Turn Up the Heat – This spicy miso broth is packed with ground pork, roasted garlic oil, and spicy chili paste. Topped with fiery chili-marinated chicken, bean sprouts, bamboo shoots, and corn, and garnished with chili threads and Sichuan pepper for a numbing spice.',
+		price: {
+			euro: '11.50',
+			yen: '1,700',
+		},
+	},
+	{
+		image: yuzuCitrus,
+		title: 'Yuzu Citrus Shoyu',
+		description:
+			'Bright, Zesty, and Bold – Discover the unique taste of citrus with this soy-based broth, paired with tender chicken chashu and topped with wild mushrooms and bok choy. A yuzu-infused chili oil, lemon zest, and microgreens add a refreshing twist to every bite.',
+		price: {
+			euro: '11.50',
+			yen: '1,700',
+		},
+	},
+	{
+		image: smokehouseRamen,
+		title: 'Smokehouse Ramen',
+		description:
+			'For the BBQ Lovers – Smoky soy and beef broth create a robust flavor base for thick-cut braised short rib, smoked egg, and grilled enoki mushrooms. Topped with house-pickled daikon, crispy onions, and a dusting of smoked paprika for a hearty, smoky experience.',
+		price: {
+			euro: '14.00',
+			yen: '2,100',
+		},
+	},
+	{
+		image: temptestVeggineRamen,
+		title: 'Tempest Veggie Ramen',
+		description:
+			'A Storm of Flavors – A miso and kombu broth loaded with fresh veggies, tofu cubes, shiitake mushrooms, sweet corn, and grilled cherry tomatoes. Topped with basil leaves, sesame seeds, and a drizzle of black garlic oil for a deep, savory taste.',
+		price: {
+			euro: '10.50',
+			yen: '1,600',
+		},
+	},
+	{
+		image: phoenixShio,
+		title: 'Phoenix Shio Ramen',
+		description:
+			'A Light and Fiery Revival – A delicate, salt-based shiobroth with a spicy twist. Topped with grilled chicken thigh, baby spinach, bamboo shoots, and sweet corn. Garnished with red pepper flakes, sesame seeds, and a hint of lime for a balanced,refreshing flavor.',
+		price: {
+			euro: '11.00',
+			yen: '1,650',
+		},
+	},
+];
+
 const createMenuCard = ({ image, title, description, price }) => {
 	const card = document.createElement('article');
 	const cardImgContainer = document.createElement('div');
@@ -15,6 +78,7 @@ const createMenuCard = ({ image, title, description, price }) => {
 	const cardTitle = document.createElement('h3');
 	const cardDescription = document.createElement('p');
 	const cardPrice = document.createElement('p');
+	const cardPriceBoldWrapper = document.createElement('strong');
 
 	card.className = 'menu-card';
 	cardImgContainer.className = 'menu-card-image';
@@ -24,11 +88,12 @@ const createMenuCard = ({ image, title, description, price }) => {
 	cardImage.src = image;
 	cardTitle.textContent = title;
 	cardDescription.textContent = description;
-	cardPrice.textContent = `Price : €${price.euro} / ¥${price.yen}`;
+	cardPriceBoldWrapper.textContent = `Price: €${price.euro} / ¥${price.yen}`;
 
 	cardImgContainer.appendChild(cardImage);
 	cardTitleContainer.appendChild(cardTitle);
 	cardBodyContainer.appendChild(cardDescription);
+	cardPrice.appendChild(cardPriceBoldWrapper);
 	cardBodyContainer.appendChild(cardPrice);
 
 	card.append(cardImgContainer, cardTitleContainer, cardBodyContainer);
@@ -36,14 +101,29 @@ const createMenuCard = ({ image, title, description, price }) => {
 	return card;
 };
 
-const data = {
-	image: tsunamiTonkotsu,
-	title: 'Tsunami Tonkotsu',
-	description:
-		'An Oceanic Take on Tradition – Dive into a smooth,ocean-inspired broth infused with seaweed and dashi. The Tsunami Tonkotsu is topped with marinated chashu pork, soft-boiled egg, shrimp tempura, scallions, sesame seeds, and a drizzle of spicy mayo for a burst of flavor.',
-	price: {
-		euro: '13.50',
-		yen: '2,000',
-	},
-};
+export const createMenuSection = () => {
+	const menuSection = document.createElement('section');
+	menuSection.id = 'menu';
 
+	const menuHeader = document.createElement('header');
+	const headerTitle = document.createElement('h2');
+	const headerDivider = document.createElement('div');
+	headerDivider.className = 'divider';
+	const headerDescription = document.createElement('p');
+
+	headerTitle.textContent = 'Our Menu';
+	headerDescription.textContent =
+		'Experience the taste of Japan with our signature ramen creations. We gladly accept both Euros (€) and Yen (¥) for your convenience.';
+
+	menuHeader.appendChild(headerTitle);
+	menuHeader.appendChild(headerDivider);
+	menuHeader.appendChild(headerDescription);
+	menuSection.appendChild(menuHeader);
+
+	data.forEach((item) => {
+		const menuItem = createMenuCard(item);
+		menuSection.appendChild(menuItem);
+	});
+
+	return menuSection;
+};
